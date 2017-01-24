a9b4da63243a451ba51a17ea07973ecdf73a9a6f
Jakub <kubawl.91@gmail.com>
2017-01-24 13:30:37 +0100

    add manager class

--- manager.py
+++ manager.py
@@ -1,2 +1,14 @@
-class Manager:
-    pass
+from user import User
+
+
+class Manager(User):
+    """
+    Class manager
+    parent class: User
+    """
+
+    def __init__(self, name, surname, email, password):
+        User.__init__(self, name, surname, email, password)
+
+    def view_manager_list(self):
+        pass
