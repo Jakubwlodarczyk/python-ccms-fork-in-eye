62a1505af2602e889a4bf230ca19a091a3b9326c
Jakub <kubawl.91@gmail.com>
2017-01-24 16:12:19 +0100

    print menu mentor

--- mentor_menu.py
+++ mentor_menu.py
@@ -1,9 +1,16 @@
-from menu import Menu
+from menu import *
+from ui import *
 
 
 class MentorMenu(Menu):
 
     @staticmethod
     def choose_option():
+        title = 'Mentor menu'
+        list_options = ['Check the list of students', 'Add an assignment',
+                        'Grade an assignment submitted by students', 'Check attendance of students',
+                        'Add a student to a class', 'Remove a student from class', "Edit student's data"]
+        Ui.print_menu(title, list_options, 'Exit')
+
 
 
