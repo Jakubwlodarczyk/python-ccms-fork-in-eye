8d23be8ee8cc6667a8f92458f62cf7cfc3ca036d
fulloffish <tunczykzcytryna@gmail.com>
2017-01-25 14:21:02 +0100

    empty module

--- student.py
+++ student.py
@@ -4,14 +4,3 @@ from user import User
 class Student(User):
 
     student_list = []
-
-
-    def __init__()
-    __init__(self, name, surname, email, password)
-
-
-    def submit an assignment(self):
-        pass
-
-    def view my grades(self):
-        pass
