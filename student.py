7f558e1522af1b1ea41af75f5aa8790379d33d25
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-01-25 10:16:47 +0100

    changes2

--- student.py
+++ student.py
@@ -2,12 +2,22 @@ from user import User
 
 
 class Student(User):
-    def __init__()
-    __init__(self, name, surname, email, password)
+    def __init__(self, attendance, id)
+     User.__init__(self, name, surname, email, password)
+        self.attendance = attendance
+        self.id = id
 
+    def submit an assignment(self, assignment):
+        pass
+        print('Assignment has been submitted')
 
-    def submit an assignment(self):
+    def view my grades(self, grades):
         pass
 
-    def view my grades(self):
-        pass
+
+    # zmiana nazwy z submit assighnent na sent submission
+    # ta metoda musi otworzyc plik csv submissions ale
+    # przy otwieraniu nie usuwac wszystkiego, tylko dopisywac
+    # pozniej zapisac w tym pliku csv to, co wymaga submission csv
+
+    
