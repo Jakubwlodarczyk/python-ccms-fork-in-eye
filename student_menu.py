f94632fc93a0c95dc6ebf6a55bb9ef4d39cf38e4
ika.grabon <ika.grabon@gmail.com>
2017-02-09 14:49:23 +0100

    change menu db

--- student_menu.py
+++ student_menu.py
@@ -37,9 +37,6 @@ class StudentMenu:
                     Ui.print_message("{}: {}".format(row[0], str(row[1])))
                 print("\n")
             elif option == '0':
-
-
                 Common.write_submission_to_db('database.db', Submission.submission_list)
-
                 sys.exit()
                 Ui.print_message('There is no such option.')
