030c21d8eb2473383003112dbee96ac0083cafbb
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-03-06 15:17:40 +0100

    assignments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models.student import Student
+from models.assignments import Assignments
 app = Flask(__name__)
 
 @app.route("/students")
@@ -7,6 +8,13 @@ def students_list():
     """ Shows list of students """
     students = Student.students_all()
     return render_template("show_students_list.html", students=students)
+
+
+@app.route("/assignments")
+def assignments_list():
+    """ Shows list of students """
+    assignments = Assignments.assignments_all()
+    return render_template("show_assignments.html", assignments=assignments)
 
 
 if __name__ == "__main__":
