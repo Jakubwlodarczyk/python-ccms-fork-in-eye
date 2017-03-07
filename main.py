300066d9eb96424baf0d2c627fe43651d892c03a
fulloffish <tunczykzcytryna@gmail.com>
2017-03-07 10:13:57 +0100

    showing people works

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
-from models.student import Student
+from models.model import Model
 app = Flask(__name__)
 
 @app.route("/students")
 def students_list():
     """ Shows list of students """
-    students = Student.students_all()
+    students = Model.students_get_all()
     return render_template("show_students_list.html", students=students)
+
+@app.route("/mentors")
+def mentors_list():
+    """ Shows list of mentors """
+    mentors = Model.mentors_get_all()
+    return render_template("show_mentors_list.html", mentors=mentors)
 
 
 if __name__ == "__main__":
