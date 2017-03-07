74674be29e68a8a13afff7ae1386150274d62c81
ika.grabon <ika.grabon@gmail.com>
2017-03-07 10:14:31 +0100

    add submission list method in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models.student import Student
+from models.submission import Submission
 app = Flask(__name__)
 
 @app.route("/students")
@@ -7,6 +8,13 @@ def students_list():
     """ Shows list of students """
     students = Student.students_all()
     return render_template("show_students_list.html", students=students)
+
+@app.route("/submissions")
+def submissions_list():
+    """Shows list of submissions"""
+    submissions = Submission.submission_all()
+    return render_template("submission_table.html", submissions=submissions)
+
 
 
 if __name__ == "__main__":
